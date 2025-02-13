78389f77eb30652a3e62ec0ef90876900e1bd46f
afdanm <afdanmaulana1777@gmail.com>
2025-02-13 19:40:19 +0700

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
 
-Route::resource('products', ProductController::class);
-Route::resource('categories', CategoryController::class);
+// Halaman Utama
+Route::get('/', function () {
+    return view('home'); // Tampilkan view home.blade.php
+});
+
+// Route untuk Produk
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
+Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+
+// Route untuk Kategori
+Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
+Route::get('/categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
+Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
